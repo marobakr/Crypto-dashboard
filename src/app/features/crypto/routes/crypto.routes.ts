d77318aa797a8c78da2f6b7bb2ec813cd315d641
Marwan Abubakr <marwanabubakr0o.o0@gmail.com>
2025-11-13 17:01:48 +0200

    ref(crypto-id): use resolver instead of direct subscription in component

--- src/app/features/crypto/routes/crypto.routes.ts
+++ src/app/features/crypto/routes/crypto.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { cryptoDetailsResolver } from '../resolvers/crypto-details-resolver';
 
 export default [
   {
@@ -9,6 +10,9 @@ export default [
 
   {
     path: ':slug',
+    resolve: {
+      coins: cryptoDetailsResolver,
+    },
     loadComponent: () => import('../pages/crypto-id/crypto-id').then((c) => c.CryptoId),
   },
 ] as Routes;
