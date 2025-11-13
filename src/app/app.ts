f5a42d02b58a3240718992986669e3c9460bc0a0
Marwan Abubakr <marwanabubakr0o.o0@gmail.com>
2025-11-13 15:25:15 +0200

    init theme

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { Topbar } from './core/components/topbar/topbar';
+import { ThemeService } from './core/services/theme';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,20 @@ import { Topbar } from './core/components/topbar/topbar';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('crypto-dashboard');
+export class App implements OnInit {
+  private themeService = inject(ThemeService);
+  isDarkMode = false;
+
+  ngOnInit(): void {
+    this.isDarkMode = this.themeService.current === 'dark-theme';
+  }
+  ngAfterViewInit(): void {
+    this.themeService.initTheme();
+  }
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+
+    this.themeService.toggleTheme();
+  }
 }
---
+++ src/app/core/services/theme.ts
@@ -0,0 +1,37 @@
+import { inject, Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private renderer: Renderer2;
+  private currentTheme: 'light-theme' | 'dark-theme' = 'light-theme';
+  private cookieService = inject(CookieService);
+
+  constructor(private rendererFactory: RendererFactory2) {
+    this.renderer = this.rendererFactory.createRenderer(null, null);
+  }
+
+  initTheme(): void {
+    const savedTheme = this.cookieService.get('web-theme') as 'light-theme' | 'dark-theme';
+    if (savedTheme) {
+      this.currentTheme = savedTheme;
+    }
+    this.renderer.addClass(document.body, this.currentTheme);
+  }
+
+  toggleTheme(): void {
+    const newTheme = this.currentTheme === 'light-theme' ? 'dark-theme' : 'light-theme';
+
+    this.renderer.removeClass(document.body, this.currentTheme);
+    this.renderer.addClass(document.body, newTheme);
+
+    this.currentTheme = newTheme;
+    this.cookieService.set('app-theme', newTheme);
+  }
+
+  get current(): 'light-theme' | 'dark-theme' {
+    return this.currentTheme;
+  }
+}
